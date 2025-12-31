c715a3cc2f4c8a6d64f92042b785231ce4fd1d1b
Marcelo Pontes Rodrigues <infopontes@gmail.com>
2025-12-31 13:16:59 -0300

    Center login heading and layout

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import LoginForm from '../components/LoginForm'; // 👈 Import the form component
+import '../styles/LoginPage.css';
 
 function LoginPage() {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="login-page">
       <h2>{t('menu.login')}</h2>
       <LoginForm /> {/* 👈 Use the form component here */}
     </div>
